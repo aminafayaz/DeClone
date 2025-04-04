47a257659f59092ef1a5aaeea87b7fa5f476ca5e
Amina Fayaz <aminafayaz1077@gmail.com>
2025-04-05 00:11:45 +0530

    added label and title

--- gui.py
+++ gui.py
@@ -2,4 +2,10 @@ import tkinter as tk
 
 def launch_gui():
     root = tk.Tk()
+
+    root.geometry("800x500")
+    root.title("DeClone")
+
+    label = tk.Label(root,text="Choose a folder",font=("Arial",20))
+    label.pack(padx=10,pady=10)
     root.mainloop() #calls the main loop of tkinterr
